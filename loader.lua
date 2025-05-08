f7c51265541e9450e74db3c87b79ec29c7987d68
ikuraxc4 <lase83914@gmail.com>
2025-05-08 12:42:44 -0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,38 @@
+
+
+-- known to work on potassium, wave, awp.gg
+local isLimited = ...
+local environment = identifyexecutor and identifyexecutor() or ""
+local source = game:HttpGet("https://raw.githubusercontent.com/ikuraxc4/test/refs/heads/main/" .. (isLimited and "sourcepf.lua" or "sourcepf.lua"))
+if getfflag and string.find(string.lower(tostring(getfflag("DebugRunParallelLuaOnMainThread"))), "true") and not executed then
+    loadstring(source)()
+elseif string.find(environment, "AWP") ~= nil and not executed then
+    for _, v in getactors() do
+        run_on_actor(v, [[
+            for _, func in getgc(false) do
+                if type(func) == "function" and islclosure(func) and debug.getinfo(func).name == "require" and string.find(debug.getinfo(func).source, "ClientLoader") then
+                    ]] .. source .. [[
+                    break
+                end
+            end
+        ]])
+    end
+elseif environment == "Wave" and not executed then
+    run_on_actor(getdeletedactors()[1], source)
+elseif environment == "Nihon" and not executed then
+    for _, actor in getactorthreads() do
+        run_on_thread(actor, [[
+            for _, func in getgc(false) do
+                if type(func) == "function" and islclosure(func) and debug.getinfo(func).name == "require" and string.find(debug.getinfo(func).source, "ClientLoader") then
+                    ]] .. source .. [[
+                    break
+                end
+            end
+        ]])
+    end
+else
+    queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/ikuraxc4/test/refs/heads/main/hook.lua") .. "task.wait(5);" .. source)
+    setfflag("DebugRunParallelLuaOnMainThread", "True")
+    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
+end
+getgenv().executed = true
